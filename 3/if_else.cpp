fa7e02d19ae67e7aeff8981db2c11a7cbc6416ee
gahyun615 <gahy615@kentech.ac.kr>
2025-03-21 00:45:02 +0000

    if-else

---
+++ 3/if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int x{};
+    cout << "Enter a number: ";
+    cin >> x;
+    if (x >= 0) {
+        cout << "You entered a positive integer!" << endl;
+    } else {
+        cout << "You entered a negative integer!" << endl;
+    }
+    return 0;
+}
