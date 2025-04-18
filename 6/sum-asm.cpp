e16bb447157a240889d886fc007da943904a0d35
gahyun615 <gahy615@kentech.ac.kr>
2025-04-18 00:48:42 +0000

    더 효율적으로

--- 6/sum-asm.cpp
+++ 6/sum-asm.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int sum(int a, int b)
 {
     __asm__(
-        "mov %rsp, %rbp\n"
-        "mov %edi, -0x4(%rbp)\n"
-        "mov %esi, -0x8(%rbp)\n"
-        "mov -0x4(%rbp), %edx\n"
-        "mov -0x8(%rbp), %edx\n"
-        "add %edx, %eax\n"
+        // "mov %rsp, %rbp\n"
+        // "mov %edi, -0x4(%rbp)\n"
+        // "mov %esi, -0x8(%rbp)\n"
+        // "mov -0x4(%rbp), %edx\n"
+        // "mov -0x8(%rbp), %edx\n"
+        // "add %edx, %eax"
+
+        "add %esi, %edi\n"
+        "mov %edi, %eax"
     );
 }
 
