c60b0c00b78adf98b92ae0ded0c5c79b30108709
gahyun615 <gahy615@kentech.ac.kr>
2025-04-18 00:41:57 +0000

    sum

---
+++ 6/sum-asm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    __asm__(
+        "mov %rsp, %rbp\n"
+        "mov %edi, -0x4(%rbp)\n"
+        "mov %esi, -0x8(%rbp)\n"
+        "mov -0x4(%rbp), %edx\n"
+        "mov -0x8(%rbp), %edx\n"
+        "add %edx, %eax\n"
+    );
+}
+
+int main()
+{
+    int a, b, c;
+    a = 3; b = 5;
+    c = sum(a, b);
+    cout << "c: " << c << endl;
+    return 0;
+}
